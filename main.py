4728620f6292fbf27d4004fadae5b900805d84cf
Branta <56791945+AfkBranta@users.noreply.github.com>
2021-05-16 09:47:24 -0400

    skin grabber :flushed:

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import urllib.request, json
+import urllib.request, json, webbrowser
 
 with urllib.request.urlopen("https://2bqueue.info/queue") as url:
     queueCount = json.loads(url.read().decode())
@@ -6,9 +6,12 @@ with urllib.request.urlopen("https://2bqueue.info/queue") as url:
 with urllib.request.urlopen("https://2bqueue.info/players") as url:
     playerNames = json.loads(url.read().decode())
 
-print('1: Queue Length')
-print('2: Players in Queue')
-print('3: Players in Server')
+print("""
+1: Queue Length
+2: Players in Queue
+3: Players in Server
+4: Skin Downloader
+""")
 input = input('What would you like to do? ')
 if input == '1':
     print('Priority Queue Length: ' + str(queueCount['prio']))
@@ -18,5 +21,8 @@ elif input == '2':
     print('Players in Regular Queue: ' + str(playerNames['queue']))
 elif input == '3':
     print('Players in Server: ' + str(playerNames['server']))
+elif option == '4':
+    skin = input('Input Username: ')
+    webbrowser.open('https://minotar.net/skin/' + skin, new=2)
 else:
     print('That is not an option! ')
